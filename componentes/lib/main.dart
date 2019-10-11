f2cfaa8dd58ef1e84ec041fb3b40a71422fc2818
JosueHernandezR <justinhernandez054@gmail.com>
2019-10-11 00:06:58 -0500

    Uso de Listtiles y creacion de nuevos metodos para que el código no se vea grotesco o dificil de leer

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
+
+
+import 'package:componentes/src/pages/home_page.dart';
+//import 'package:componentes/src/pages/home_temp.dart';
  
 void main() => runApp(MyApp());
  
@@ -9,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp(),
+      home: HomePage(),
     );
   }
 }
