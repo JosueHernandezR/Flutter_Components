130ccc0d176ef1a2410c309c13baa8ac90e53543
JosueHernandezR <justinhernandez054@gmail.com>
2019-10-14 18:43:18 -0500

    Animaciones de containers

--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
 
 //import 'package:componentes/src/pages/home_temp.dart';
 
@@ -15,5 +16,6 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
         'alert'   : ( BuildContext context ) => AlertPage(),
         'avatar'  : ( BuildContext context ) => AvatarPage(),
         'card'    : ( BuildContext context ) => CardPage(),
+        'animatedContainer' : ( BuildContext content ) => AnimatedContainerPage(),
       };
 }
