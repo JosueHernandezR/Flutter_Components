b132b61d76bdef948d36571c242a60832219ab1a
JosueHernandezR <justinhernandez054@gmail.com>
2019-10-14 14:42:35 -0500

    Configuración de alertas

--- componentes/lib/src/pages/alert_page.dart
+++ componentes/lib/src/pages/alert_page.dart
@@ -9,12 +9,58 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlert( context ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.arrow_back ), 
         onPressed: () {
           Navigator.pop( context );
         },
       ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context){
+
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Título'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la caja de alerta'),
+              FlutterLogo( size: 100 )
+
+
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar') ,
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Aceptar') ,
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+
+      }
     );
   }
 }
