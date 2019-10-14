3a5ae5a283c8b5bd3eeecdf0069dca447a4556a4
JosueHernandezR <justinhernandez054@gmail.com>
2019-10-14 15:02:11 -0500

    Configuraciones de avatar

--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -6,9 +6,39 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS83LBvYuTVQy8YFNpQcpOeinnrkQfrJHgzrrPC8h-dMDQmiktX'),
+              radius: 25.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('JD'),
+              backgroundColor: Colors.amber,
+            ),
+          )
+        ],
       ),
+
+
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS83LBvYuTVQy8YFNpQcpOeinnrkQfrJHgzrrPC8h-dMDQmiktX'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
+
+    
     );
   }
 }
