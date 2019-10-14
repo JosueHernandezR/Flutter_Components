fd8c68a87a44508956175a801d5de19f3b8aa94c
JosueHernandezR <justinhernandez054@gmail.com>
2019-10-14 13:55:54 -0500

    Configuración de imagen temporal en lo que carga una imagen

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -12,12 +12,14 @@ class CardPage extends StatelessWidget {
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
           _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2(),
         ],
       ),
     );
   }
 
-   Widget _cardTipo1() {
+  Widget _cardTipo1() {
 
      return Card(
        child: Column(
@@ -43,6 +45,30 @@ class CardPage extends StatelessWidget {
          ],
        ),
      );
+
+  }
+
+  Widget _cardTipo2() {
+
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage('https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          /* Image(
+            image: NetworkImage('https://static.photocdn.pt/images/articles/2017_1/iStock-545347988.jpg'),
+          ), */
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra aliquam tortor, a convallis justo convallis eu. Nulla quam quam, consectetur ut ultrices eget, malesuada et sem.')),
+        ],
+      ),
+    );
 
   }
 }
